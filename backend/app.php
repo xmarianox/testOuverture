4246230995634a614ac635037200ae92113f5e82
Mariano Molina <mariano@contenidos-digitales.com>
2016-04-01 15:01:43 -0300

    Fix json_enconde() errors

--- backend/app.php
+++ backend/app.php
@@ -9,35 +9,68 @@ $db 	  = 'ouverture';
 $host 	  = 'localhost';
 $port 	  = 8889;
 
-// connection
-$mysqli = new mysqli("$host:$port", $user, $password, $db);
+function getLatest() {
+	global $host, $port, $user, $password, $db;
 
-// Check connection
-if ($mysqli->connect_error) {
-	die("Connection failed: " . $mysqli->connect_error);
-}
-// set array
-$json = array();
+	// connection
+	$mysqli = new mysqli("$host:$port", $user, $password, $db);
 
-if (empty($_GET['products'])) {
-	die(json_encode(die('connection error')));
-}
+	// Check connection
+	if ($mysqli->connect_error):
+		die("Connection failed: " . $mysqli->connect_error);
+	endif
 
-if ($_GET['products'] == 'getlatest') {
-	if ($result = $mysqli->query("SELECT * FROM products")) {
-	    while($row = $result->fetch_array(MYSQLI_ASSOC)) {
-	    	$prod = array(
-	    		'id' => $row['id'],
-	    		'name' => $row['name'],
-	    		'description' => $row['description']
-	    	);
-	    	array_push($json, $prod);
-	    }
-	    var_dump($json);
-	    //echo json_encode($json, JSON_FORCE_OBJECT);
-	}
+	// set array
+	$products = array();
+
+	if ($result = $mysqli->query("SELECT * FROM products")):
+		while($row = $result->fetch_array(MYSQLI_ASSOC)):
+			$prod = array(
+				'id' => $row['id'],
+				'name' => utf8_encode($row['name']),
+				'description' => utf8_encode($row['description'])
+			);
+			array_push($products, $prod);
+		endwhile;
+
+		// var_dump($products);
+		$json = json_encode($products, JSON_FORCE_OBJECT);
+		switch (json_last_error()):
+			case JSON_ERROR_NONE:
+				return $json;
+				break;
+			case JSON_ERROR_DEPTH:
+				return 'Maximum stack depth exceeded'; // or trigger_error() or throw new Exception()
+				break;
+			case JSON_ERROR_STATE_MISMATCH:
+				return 'Underflow or the modes mismatch'; // or trigger_error() or throw new Exception()
+				break;
+			case JSON_ERROR_CTRL_CHAR:
+				return 'Unexpected control character found';
+				break;
+			case JSON_ERROR_SYNTAX:
+				return 'Syntax error, malformed JSON'; // or trigger_error() or throw new Exception()
+				break;
+			case JSON_ERROR_UTF8:
+				$clean = utf8_encode($products);
+				return safe_json_encode($clean);
+				break;
+			default:
+				return 'Unknown error'; // or trigger_error() or throw new Exception()
+				break;
+		endswitch;
+	endif;
+
 	$result->close();
 	$mysqli->close();
 }
+
+if (empty($_GET['products'])):
+	echo json_encode(die('wrong parameters'));
+endif;
+
+if ($_GET['products'] == 'getlatest'):
+	echo getLatest();
+endif;
 
 ?>
